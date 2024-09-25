c90480c5ce6504d1024bbcc15c0fb4b4e528d877
amitdutta <duttaamit356@gmail.com>
2024-09-25 16:23:20 +0530

    Setup firebase and routes

---
+++ src/pages/myphotos/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface IMyPhotosProps {}
+
+const MyPhotos: React.FunctionComponent<IMyPhotosProps> = (props) => {
+  return <div className="bg-slate-600 text-fuchsia-800">MyPhotos</div>;
+};
+
+export default MyPhotos;
